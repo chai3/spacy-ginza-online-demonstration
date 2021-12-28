775032ab0d6cacc49c42926ea678fafed57ea5c9
chai3 <16031581+chai3@users.noreply.github.com>
2021-12-29 01:53:36 +0900

    support ginza

--- main.py
+++ main.py
@@ -1,26 +1,79 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+from ginza.analyzer import Analyzer
+# https://github.com/tiangolo/fastapi/issues/1663#issuecomment-906817935
+# from fastapi.middleware.cors import CORSMiddleware
+from starlette.middleware.cors import CORSMiddleware
+from starlette.requests import Request
+import time
+import threading
+
+app = FastAPI(
+    debug=True
+)
+
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=["*"],
+#     allow_methods=["*"],
+# )
 
 
-@app.get("/")
-def root():
-    return {"message": "hello world"}
-
-
-@app.get("/test")
-def test(text: str = "銀座でランチをご一緒しましょう。", model_name: str = "ja_ginza"):
-    import spacy
-    # nlp = spacy.load("ja_ginza")
-    # nlp = spacy.load("ja-ginza-electra")
-    nlp = spacy.load(model_name)
-    doc = nlp(text)
+@app.get("/analyze")
+def analyze(
+    request: Request,
+    text: str = "",  # "銀座でランチをご一緒しましょう。"
+    ignore_lf: bool = False,
+    split_mode: str = "C",  # A,B,C
+    output_format: str = "conllu",  # conllu, cabocha, mecab, json
+):
+    print(
+        f"analyze {threading.get_ident()} {request} {text=} {ignore_lf=} {split_mode=} {output_format=}")
+    analyzer = Analyzer(
+        model_name_or_path="ja_ginza",
+        split_mode=split_mode,
+        hash_comment="print",
+        output_format=output_format,
+        require_gpu=-1,
+        disable_sentencizer=False,
+        use_normalized_form=False,
+    )
     results = []
-    for sent in doc.sents:
-        for token in sent:
-            data = [token.i, token.orth_, token.lemma_, token.pos_, token.tag_, token.dep_, token.head.i]
-            print(f"{data}")
-            results.append(", ".join([str(v) for v in data]))
-        print("EOS")
-    del nlp
+    analyzer.set_nlp()
+
+    text = text.replace("\t", " ").replace("\r", "")
+    if ignore_lf:
+        text = text.replace("\n", "")
+
+    for text_line in text.splitlines():
+        results.append(analyzer.analyze_line(text_line))
+
+    print(f"analyze {threading.get_ident()} {request} {len(results)=}")
     return {"results": results}
+
+
+@app.get("/warmup")
+def warmUpHeroku(request: Request):
+    print(f"warmUpHeroku {threading.get_ident()} {request}")
+    return {"message": "success"}
+
+
+@app.get("/test_timeout")
+def testTimeout(request: Request):
+    for i in range(360):  # 60 min
+        print(f"sleep {threading.get_ident()} {i} {request}")
+        time.sleep(10)
+
+    return {"message": "success"}
+
+
+@app.get("/test_raise")
+def testTimeout():
+    raise RuntimeError("test raise")
+
+
+app = CORSMiddleware(
+    app=app,
+    allow_origins=["*"],
+    allow_methods=["*"],
+)
